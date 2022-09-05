193a1a1a9706784b79b84d57da59310b315bb815
Kyle Thomas <kylejwthomas@gmail.com>
2022-09-05 19:57:54 +0900

    added more inputs

--- input.js
+++ input.js
@@ -1,17 +1,24 @@
 export default class InputHandler {
   constructor(){
     this.lastKey = '';
-    window.addEventListener('keydown', function(e){
-      console.log(e.key);
+    window.addEventListener('keydown', (e) => {
       switch(e.key){
         case "ArrowLeft":
           this.lastKey = "PRESS left";
+          break
+        case "ArrowRight":
+          this.lastKey = "PRESS right";
+          break
       }
     });
-    window.addEventListener('keyup', function(e){
+    window.addEventListener('keyup', (e) => {
       switch(e.key){
         case "ArrowLeft":
           this.lastKey = "RELEASE left";
+          break
+        case "ArrowRight":
+          this.lastKey = "RELEASE right";
+          break
       }
     });
   }
