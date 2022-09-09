157e1b94199762f0c83b7428415549c57391e0e0
Kyle Thomas <kylejwthomas@gmail.com>
2022-09-09 17:42:18 +0900

    added set state

--- player.js
+++ player.js
@@ -15,4 +15,7 @@ export default class Player {
   draw(context){
     context.drawImage(this.image, this.width * this.frameX, this.height * this.frameY, this.width, this.height, this.x, this.y, this.width, this.height);
   }
+  setState(state){
+    this.currentState = this.states[state];
+  }
 }
