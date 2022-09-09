fdc681c09ec311aba59e51c1c877bad8b3820341
Kyle Thomas <kylejwthomas@gmail.com>
2022-09-09 18:11:31 +0900

    edited utils

--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
-export function drawStatusText(context, input){
+export function drawStatusText(context, input, player){
   context.font = '10px Helvetica';
   context.fillText('Last input: ' + input.lastKey, 10, 20);
+  context.fillText('Active state: ' + player.currentState.state);
 }
