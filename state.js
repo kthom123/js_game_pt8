aee8e2991f6532fce2e80af0ee94999e9a7f4906
Kyle Thomas <kylejwthomas@gmail.com>
2022-09-10 12:36:40 +0900

    added more runnings states

--- state.js
+++ state.js
@@ -22,8 +22,9 @@ export class StandingLeft extends State {
     this.player.frameY = 1;
   }
   handleInput(input){
-    if (input === 'PRESS right') this.player.setState(states.STANDING_RIGHT);
-    else if (input === 'PRESS down') this.player.setState(states.SITTING_LEFT);
+    if (input === 'PRESS right') this.player.setState(states.RUNNING_RIGHT);
+    else if (input === 'PRESS left') this.player.setState(states.RUNNING_LEFT);
+    else if (input === 'PRESS down') this.player.setState(states.RUNNING_LEFT);
   }
 }
 
@@ -36,7 +37,8 @@ export class StandingRight extends State {
     this.player.frameY = 0;
   }
   handleInput(input){
-    if (input === 'PRESS left') this.player.setState(states.STANDING_LEFT);
+    if (input === 'PRESS left') this.player.setState(states.RUNNING_LEFT);
+    else if (input === 'PRESS right') this.player.setState(states.RUNNING_RIGHT);
     else if (input === 'PRESS down') this.player.setState(states.SITTING_RIGHT);
   }
 }
