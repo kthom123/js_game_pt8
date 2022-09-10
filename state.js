c6f96ad0ee5037684b94063b17fd71c4a8898878
Kyle Thomas <kylejwthomas@gmail.com>
2022-09-10 13:06:26 +0900

    edited jumping

--- state.js
+++ state.js
@@ -118,10 +118,10 @@ export class JumpingLeft extends State {
   }
   enter(){
     this.player.frameY = 3;
-    this.player.vy -= 20;
+    this.player.vy -= 10;
   }
   handleInput(input){
-
+    if (input === 'PRESS right') this.player.setState(states.JUMPING_RIGHT);
   }
 }
 
@@ -132,9 +132,9 @@ export class JumpingRight extends State {
   }
   enter(){
     this.player.frameY = 2;
-    this.player.vy -= 20;
+    this.player.vy -= 10;
   }
   handleInput(input){
-
+    if (input === 'PRESS left') this.player.setState(states.JUMPING_LEFT);
   }
 }
