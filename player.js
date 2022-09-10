341c1bac2765f515e554cbcbd05aaed770f5a5a2
Kyle Thomas <kylejwthomas@gmail.com>
2022-09-10 13:19:28 +0900

    added falling states

--- player.js
+++ player.js
@@ -35,6 +35,7 @@ export default class Player {
     } else {
       this.vy = 0;
     }
+    if (this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height;
   }
   setState(state){
     this.currentState = this.states[state];
--- state.js
+++ state.js
@@ -7,6 +7,8 @@ export const states = {
   RUNNING_RIGHT: 5,
   JUMPING_LEFT: 6,
   JUMPING_RIGHT: 7,
+  FALLING_LEFT: 8,
+  FALLING_RIGHT: 9,
 }
 
 class State {
@@ -139,6 +141,34 @@ export class JumpingRight extends State {
   }
   handleInput(input){
     if (input === 'PRESS left') this.player.setState(states.JUMPING_LEFT);
+    else if (this.player.onGround()) this.player.setState(states.STANDING_RIGHT);
+  }
+}
+
+export class FallingLeft extends State {
+  constructor(player){
+    super('FALLING LEFT');
+    this.player = player;
+  }
+  enter(){
+    this.player.frameY = 5;
+  }
+  handleInput(input){
+    if (input === 'PRESS right') this.player.setState(states.FALLING_RIGHT);
+    else if (this.player.onGround()) this.player.setState(states.STANDING_LEFT);
+  }
+}
+
+export class FallingRight extends State {
+  constructor(player){
+    super('FALLING RIGHT');
+    this.player = player;
+  }
+  enter(){
+    this.player.frameY = 4;
+  }
+  handleInput(input){
+    if (input === 'PRESS left') this.player.setState(states.FALLING_LEFT);
     else if (this.player.onGround()) this.player.setState(states.STANDING_RIGHT);
   }
 }
