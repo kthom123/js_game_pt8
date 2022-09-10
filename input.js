d5aef8832f7f7d69b0bb8475c20b2627067ec35a
Kyle Thomas <kylejwthomas@gmail.com>
2022-09-10 11:38:47 +0900

    added arrow up key

--- input.js
+++ input.js
@@ -12,6 +12,9 @@ export default class InputHandler {
         case "ArrowDown":
           this.lastKey = "PRESS down";
           break
+        case "ArrowUp":
+          this.lastKey = "PRESS up";
+          break
       }
     });
     window.addEventListener('keyup', (e) => {
@@ -24,6 +27,9 @@ export default class InputHandler {
           break
         case "ArrowDown":
           this.lastKey = "RELEASE down";
+          break
+        case "ArrowUp":
+          this.lastKey = "RELEASE up";
           break
       }
     });
